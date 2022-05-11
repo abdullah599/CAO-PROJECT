cb4939faaf83db43081ce5f69e5935df644ce567
Abdullah Shakeel <abdullahshakeel599@gmail.com>
2022-05-12 03:01:36 +0500

    1. Set up I2C LCD. 2. Configured Firebase

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,113 @@
 //Libraries
 #include <Arduino.h>
 #include<WiFi.h>
+#include <LiquidCrystal_I2C.h>
+#include <Firebase_ESP_Client.h>
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+
+//Declaration
+
+//for wifi
+#define ssid  "Abdullah"
+#define password "multan123"
+
+// For firebase
+#define API_KEY "AIzaSyAnyGxL0rdgdrYzq2zJCs2bBU5NFUIHLBo"
+#define DATABASE_URL "https://cao-project-8bf71-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+//Define Firebase Data object
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+
+//Function declaration
+void initWiFi();
 
 
 
-const char *ssid = "Abdullah";
-const char *password = "multan123";
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+void setup() {
+  Serial.begin(916200);
+  //WiFi initialization
+  initWiFi();
+  //LCD initialization
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();// Make sure backlight is on
+
+
+  lcd.setCursor(0,0);   //Set cursor to character 0 on line 0
+  lcd.print("Connected with:");
+  
+  lcd.setCursor(0,1);   //Move cursor to character 0 on line 1
+  lcd.print(WiFi.SSID());
+
+  
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Connected to fb");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 500 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setString(&fbdo, "testdata/SSID", WiFi.SSID())){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+      lcd.setCursor(0, 1);
+      lcd.print("Data sent to firebase");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    count++;
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, "testdata/float", 0.01 + random(0,100))){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+}
+
+
+//For initializing wifi connection
 void initWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -15,59 +116,5 @@ void initWiFi() {
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(1000);
-  }
-  Serial.println(WiFi.localIP());
-  Serial.println(WiFi.SSID());
+  }  
 }
-
-void setup() {
-  Serial.begin(921600);
-  initWiFi();
-}
-void loop() {
- 
-  
-}
-
-
-// FOR SCANNING AVAILABLE SSIDs
-
-/*void setup() {
-  Serial.begin(921600);
-
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  Serial.println("Setup done");
-}
-
-void loop() {
-  Serial.println("scan start");
-
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0) {
-      Serial.println("no networks found");
-  } else {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.println(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-      delay(10);
-    }
-  }
-  Serial.println("");
-
-  // Wait a bit before scanning again
-  delay(5000);
-}*/
